b683859193eee75e99ad714e83eadf47ab25b00a
Giovanni Lodi <giovanni.lodi42@gmail.com>
2017-09-18 22:07:36 +1000

    Run migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170918114300) do
+ActiveRecord::Schema.define(version: 20170918120649) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 20170918114300) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_favourites_on_user_id"
   end
 
   create_table "items", force: :cascade do |t|
@@ -33,4 +35,5 @@ ActiveRecord::Schema.define(version: 20170918114300) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "favourites", "users"
 end
