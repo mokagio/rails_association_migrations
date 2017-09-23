76a8bb04c50007be10266f06af079b600b935513
Giovanni Lodi <giovanni.lodi42@gmail.com>
2017-09-23 14:47:26 +1000

    Make User has_many Favourites explicit

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should have_many :favourites }
+
+  # This test is just to ensure that I've written the migration and associations correctly.
+  # One shouldn't usually test the workings of one of it's dependencies, ActiveRecord in this case.
+  it 'can be assigned many favourites' do
+    user = User.create!
+    user.favourites.push Favourite.create!(user: user, item: Item.create!, name: 'foo')
+    user.favourites.push Favourite.create!(user: user, item: Item.create!, name: 'bar')
+
+    expect(user.favourites.length).to eq 2
+  end
 end
